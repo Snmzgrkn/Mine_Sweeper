6de8496692e0032db084e33d070f17aff49ae92d
Gürkan Sönmez <snmzgrkn@gmail.com>
2024-02-29 15:59:36 +0300

    Main Method created2.

--- MineSweeper/src/Main.java
+++ MineSweeper/src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Merhaba Dünya,Ben Gürkan main metodu çalışıyor.");
+        System.out.println("Merhaba Dünya, Ben Gürkan main metodu çalışıyor.");
     }
 }
